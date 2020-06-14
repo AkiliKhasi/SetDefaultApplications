79baeab11c027cf520a49642ff1fb0025d3c95f3
AkiliKhasi <59064515+AkiliKhasi@users.noreply.github.com>
2020-06-13 22:01:02 -0400

    Update DefaultApps.ps1

--- DefaultApps.ps1
+++ DefaultApps.ps1
@@ -1,6 +1,6 @@
 Import-Csv -Path "C:\PClist.csv" | % {
 $computer = $_.Name
 Invoke-Command -ComputerName $computer -ScriptBlock{
-      Dism.exe /online /import-DefaultAppAssociations:\\wab1\netlogon\DefaultApps.xml
+      Dism.exe /online /import-DefaultAppAssociations:\\server1\netlogon\DefaultApps.xml
      }
 }
